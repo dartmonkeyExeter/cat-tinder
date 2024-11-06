b58b5def07ea2b11fd911b83cdcc8efd33304cd8
dartmonkeyexeter <aaronhampson@exe-coll.ac.uk>
2024-11-06 11:39:26 +0000

    at the copa . copacabana . the hottest spot north of HAVANA!!

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,31 @@
 from app.main import bp
+import requests
+from flask import jsonify, request, render_template, redirect, url_for, current_app
+from dotenv import load_dotenv
+import os
 
+load_dotenv()  # Load environment variables
+
+# Use the API_KEY securely from the environment
+API_KEY = os.getenv('API_KEY')
+BASE_URL = 'https://api.thecatapi.com/v1'
 
 @bp.route('/')
 def index():
-    return 'This is The Main Blueprint'
+    # get three cats, and render the template
+    cats = redirect(url_for('main.get_cats', amount=3))
+    return render_template('index.html', cats=cats)
+
+@bp.route('/get_cats/<int:amount>', methods=['GET'])
+def get_cats(amount):
+    """Fetches a list of random cats from The Cat API"""
+    try:
+        headers = {'x-api-key': API_KEY}
+        response = requests.get(f'{BASE_URL}/images/search?limit={amount}', headers=headers)
+        if response.status_code == 200:
+            cats_data = response.json()  # Extract the JSON data from the response
+            return cats_data
+        else:
+            return jsonify({'error': 'Failed to fetch data from The Cat API'}), 500
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
